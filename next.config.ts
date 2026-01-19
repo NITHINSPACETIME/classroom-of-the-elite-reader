ce4946f76337d8e80360f33a808ce1783c92b53d
NITHINSPACETIME <166204452+NITHINSPACETIME@users.noreply.github.com>
2026-01-19 20:28:52 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     compress: true,
     images: {
         qualities: [60, 75],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048], // Removed 3840 to save bandwidth
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048], 
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
     serverExternalPackages: ['jszip'],
